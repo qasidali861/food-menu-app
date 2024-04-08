5a3345a18b20b4a51b53440cba26f928c073ca48
qasidali <qasidali768@gmail.com>
2024-04-09 03:24:18 +0500

    learned about urls and view

--- food_menu/food/urls.py
+++ food_menu/food/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', ),
-]
+    path('index', views.index, name="index"),
+    path('item', views.item, name="item"),
+] 
--- food_menu/food/views.py
+++ food_menu/food/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    
     return HttpResponse("Hello World")
+
+def item(request):
+    return HttpResponse("<h1>This is my new view</h1>")
